9552814c8e72175243e692b999aa5dbb06b0bccd
Dave Calderwood <davidhcalderwood@gmail.com>
2018-12-31 12:38:09 -0800

    Built routing for starting pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Calendar from './pages/Calendar';
+import Calculator from './pages/Calculator';
 import './App.css';
 
+
 class App extends Component {
+ 
   render() {
     return (
-      <div className="App">
+
+      <Router>
+      <div className="App">   
+        <div className="sideBar">
+        
+          <nav role="navigation">
+            <div id="menuToggle">
+
+              <ul id="menu">
+                <a href="/"><li>Home</li></a>
+                <a href="/about"><li>About</li></a>
+                <a href="/calendar"><li>Calendar</li></a>
+                <a href="/calculator"><li>Calculator</li></a>
+              </ul>
+            </div>
+          </nav>
+          
+        </div>
+
+        <div className="main">
+          {/* Route paths */}
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/calendar" component={Calendar} />
+          <Route path="/calculator" component={Calculator} />
+        
+        </div>
       </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/Calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Calculator extends Component {
+    render(){
+    return (
+        <div className="component">
+            <div className="calculator">Calculator</div>
+        </div>
+    )
+    }
+}
